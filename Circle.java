cea01e75fb06e938634635f5468145dd8ecccbdc
Swaraj Parag Kondlekar <83810295+kondlekarswaraj@users.noreply.github.com>
2021-08-31 13:53:45 +0530

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,11 @@
+package Shapes;
+
+public class Circle extends Constructor {
+
+	int radius = 20;
+
+	void area()
+	{
+	System.out.println(" The area of Circle of radius " + radius + " = " + 3.142f*radius*radius);
+	}
+}
---
+++ Square.java
@@ -0,0 +1,11 @@
+package Shapes;
+
+public class Square extends Constructor{
+
+	int side = 10;
+
+	void area()
+	{
+	System.out.println(" The area of Square of side " + side + " = " + side*side);
+	}
+}
